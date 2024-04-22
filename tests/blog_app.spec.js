fb08c9e6f46a97eee2b7eec7a1f43d8dd1cc0dc4
Martin Marchionni <tinchomarchi34@gmail.com>
2024-04-22 17:48:03 -0300

    add exercises

--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -3,8 +3,15 @@ const exp = require('constants')
 require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:5173')
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3001/api/testing/reset')
+    await request.post('http://localhost:3001/api/users', {
+      data: {
+        username: 'pepe',
+        password: '123'
+      }
+    })
+    await page.goto('http://localhost:5173/')
   })
 
   test('Login form is shown', async ({ page }) => {
@@ -21,5 +28,49 @@ describe('Blog app', () => {
     await expect(usernameInput).toBeVisible()
     await expect(passwordInput).toBeVisible()
     await expect(submit).toBeVisible()
+  })
+  describe('Login', () => {
+    test('Login fail with wrong password', async ({ page }) => {
+      const usernameInput = await page.getByTestId('username').fill('pepe')
+      const passwordInput = await page.getByTestId('password').fill('wrong')
+      const submit = await page.getByRole('button', { name: 'login' }).click()
+      await expect(page.getByText('wrong credentials')).toBeVisible()
+    })
+    test('Login working', async ({ page }) => {
+      const usernameInput = await page.getByTestId('username').fill('pepe')
+      const passwordInput = await page.getByTestId('password').fill('123')
+      const submit = await page.getByRole('button', { name: 'login' }).click()
+      await expect(page.getByText('blogs')).toBeVisible()
+    })
+  })
+  describe('When logged in', () => {
+    beforeEach(async ({ page }) => {
+      const usernameInput = await page.getByTestId('username').fill('pepe')
+      const passwordInput = await page.getByTestId('password').fill('123')
+      const submit = await page.getByRole('button', { name: 'login' }).click()
+    })
+
+    test('a new blog can be created', async ({ page }) => {
+      const newBlog = {
+        "title": "pepeblog",
+        "author": "pepito",
+        "url": "http://pepe.com"
+      }
+      await page.getByRole('button', { name: 'create new blog' }).click()
+      await page.getByTestId('title').fill(newBlog.title)
+      await page.getByTestId('author').fill(newBlog.author)
+      await page.getByTestId('url').fill(newBlog.url)
+      await page.getByRole('button', { name: 'create' }).click()
+      await expect(page.getByText(`${newBlog.title} ${newBlog.author}`)).toBeVisible()
+      await page.getByText('view').click()
+      await expect(page.getByText('0 likes')).toBeVisible()
+      const buttonlike = await page.getByRole('button', { name: 'like' }).click()
+      await expect(page.getByText('1 likes')).toBeVisible()
+      await page.on('dialog', async dialog => {
+        await dialog.accept();
+      })
+      await page.getByRole('button', { name: 'remove' }).click()
+      await expect(page.getByText(`${newBlog.title} ${newBlog.author}`)).not.toBeVisible()
+    })
   })
 })
